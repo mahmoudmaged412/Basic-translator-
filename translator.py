66017d2f237415c3e4bd74036f808be41ea5c031
Mahmoud Tourky <102033579+mahmoudmaged412@users.noreply.github.com>
2023-01-23 09:43:44 +0200

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,14 @@
+def translate(str):
+    translation = ""
+    for letter in str:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "Z"
+            else:
+                translation=translation+"z"
+        else:
+            translation = translation + letter
+
+    return translation
+
+print(translate(input("enter a phrase\n")))
